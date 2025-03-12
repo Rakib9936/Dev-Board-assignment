1a094f7dd104240a0e4e57646369556082a0bc93
Rakib9936 <md.rakib.hasan9936@gmailcom>
2025-03-13 00:45:16 +0600

    changing color button add

--- script/script.js
+++ script/script.js
@@ -1,1 +1,13 @@
-alert('tussu le lo')
+document.getElementById('change-color').addEventListener('click',function(){
+   const button=document.getElementById('change-color');
+   let r=Math.floor(Math.random()*256);
+   let g=Math.floor(Math.random()*256);
+   let b=Math.floor(Math.random()*256);
+   let getRendomcolor="rgb(" +r+ "," +g+ "," +b+")"
+   
+  document.body.style.background=getRendomcolor;
+});
+
+document.getElementById('change-page')
+
+
